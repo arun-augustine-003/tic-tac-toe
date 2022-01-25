af032b22cbcb3173fd8218c2d21e6335e00abd47
Arun Augustine <arunaugustine@rarecrew.com>
2022-01-25 23:54:37 +0530

    second commit -Removed Nebular and added PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+@Component({
+  selector: 'app-square',
+  template: `
+    <button *ngIf="!value">{{ value }}</button>
+    <button pButton type="button" class="p-button-success" *ngIf="value == 'X'">
+      {{ value }}
+    </button>
+    <button pButton type="button" class="p-button-info" *ngIf="value == 'O'">
+      {{ value }}
+    </button>
+  `,
+  styles: [
+    'button { width: 100%; height: 100%; font-size: 5em !important; padding-left:50px; }',
+  ],
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
+}
